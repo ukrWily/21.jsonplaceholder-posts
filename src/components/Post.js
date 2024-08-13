449398d835285d07a27422b033bc834d60406ff2
”ukrWily” <ukrwily@gmail.com>
2024-08-13 20:40:01 +0300

    PC final

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,12 @@
-function Post() {
-  return <div>This is a post</div>;
+function Post({ userId, id, title, body }) {
+  //
+  return (
+    <div className="post">
+      <p>userId: {userId}</p>
+      <small>id: {id}</small>
+      <h3>title: {title}</h3>
+      <p>body: {body}</p>
+    </div>
+  );
 }
 export default Post;
